4b596d2081311e9be50b54de61e4491358d2515e
Yasai <alexvidalcasado@gmail.com>
2023-12-15 09:12:49 +0100

    [ADD] secure login with jwt

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,124 +1,132 @@
 import dbQuery from "../db/dbConnection.js";
 import bcrypt from "bcrypt";
-
+import jwt from "jsonwebtoken";
 
 //Request es los que recoje la informacion que se envia, res es la respuesta que se envia
-export const usersGet = async(req, res) => {
-
-  
+export const usersGet = async (req, res) => {
   let result;
   try {
-
-    result =  await dbQuery("SELECT * FROM USUARIOS;");
-
-  }catch(e){
+    result = await dbQuery("SELECT * FROM USUARIOS;");
+  } catch (e) {
     return res.json({
-      error: "Invalid query" ,
+      error: "Invalid query",
     });
   }
-  
 
   res.json({
-
-    msg:  result,
+    msg: result,
   });
 };
 
-
-export const userDelete = async(req, res) => {
-
+export const userDelete = async (req, res) => {
   const { userId } = req.body;
-  
   let result;
   try {
-
-    result =  await dbQuery(`DELETE FROM USUARIOS WHERE Id = '${userId}';`);
-
-  }catch(e){
-    console.log(e)
+    result = await dbQuery(`DELETE FROM USUARIOS WHERE Id = '${userId}';`);
+  } catch (e) {
+    console.log(e);
     return res.json({
-      error: "Invalid query" ,
+      error: "Invalid query",
     });
   }
-  
 
   res.json({
-
-    msg:  "ok",
+    msg: "ok",
   });
 };
 
-
-export const userGet = async(req, res) => {
-
+export const userGet = async (req, res) => {
   const { userId } = req.body;
-  
+
   let result;
   try {
-
-    result =  await dbQuery(`SELECT * FROM USUARIOS WHERE Id = ${userId};`);
-
-  }catch(e){
+    result = await dbQuery(`SELECT * FROM USUARIOS WHERE Id = ${userId};`);
+  } catch (e) {
     return res.json({
-      error: "Invalid query" ,
+      error: "Invalid query",
     });
   }
-  
 
   res.json({
-
-    msg:  result,
+    msg: result,
   });
 };
 
-export const userPost = async(req, res) => {
-
-  const {userUser,userPass, userName,userType } = req.body
+export const userPost = async (req, res) => {
+  const { userUser, userPass, userName, userType } = req.body;
   const temp = req.body;
-
 
   const hashedPass = bcrypt.hashSync(userPass, 10);
 
-  
-
-  
   let result;
   try {
-
-    result =  await dbQuery(`INSERT INTO USUARIOS (User,Pass,Nom,Tipus) VALUES ('${userUser}','${hashedPass}','${userName}','${userType}');`);
-
-  }catch(e){
-    console.log(e)
+    result = await dbQuery(
+      `INSERT INTO USUARIOS (User,Pass,Nom,Tipus) VALUES ('${userUser}','${hashedPass}','${userName}','${userType}');`
+    );
+  } catch (e) {
+    console.log(e);
     return res.json({
-      error: "Invalid query" ,
+      error: "Invalid query",
     });
   }
-  
 
   res.json({
-
-    msg:  result,
+    msg: "ok",
   });
 };
 
-export const userPut = async(req, res) => {
+export const userPut = async (req, res) => {
+  const { userId, userFoto } = req.body;
 
-  const {userId, userFoto } = req.body
-  
   let result;
   try {
-
-    result =  await dbQuery(`UPDATE USUARIOS SET Foto = '${userFoto}' WHERE Id = '${userId}'`);
-
-  }catch(e){
-    console.log(e)
+    result = await dbQuery(
+      `UPDATE USUARIOS SET Foto = '${userFoto}' WHERE Id = '${userId}'`
+    );
+  } catch (e) {
+    console.log(e);
     return res.json({
-      error: "Invalid query" ,
+      error: "Invalid query",
     });
   }
-  
+
   res.json({
-    msg:  "ok",
+    msg: "ok",
   });
 };
 
+export const login = async (req, res) => {
+  const { userName, userPass } = req.body;
+
+  let result;
+  try {
+    result = await dbQuery(
+      `SELECT Id, User, Nom, Pass FROM USUARIOS WHERE User = '${userName}'`
+    );
+  } catch (e) {
+    return res.json({
+      error: "Invalid query",
+    });
+  }
+
+  if (result.length === 0) {
+    return res.status(400).json({
+      error: "Invalid user or password",
+    });
+  }
+
+  let user = result[0];
+
+  if (!bcrypt.compareSync(userPass, user.Pass)) {
+    return res.status(400).json({
+      error: "Invalid user or password",
+    });
+  }
+  const token = jwt.sign(user.Id, process.env.SECRET);
+
+  delete user.Pass;
+  res.json({
+    user,
+    token,
+  });
+};
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,18 +1,25 @@
 import { Router } from "express";
-import { userDelete, userGet, userPost, userPut, usersGet } from "../controllers/userController.js";
-import verifyJWT from "../middlewares/verifyJWT.js";
+import {
+  login,
+  userDelete,
+  userGet,
+  userPost,
+  userPut,
+  usersGet,
+} from "../controllers/userController.js";
 
 //Creamos la ruta
 const router = Router();
 
-
-//router.get("/", [verifyJWT], usersGet); ejemplo verificar 
+//router.get("/", [verifyJWT], usersGet); ejemplo verificar
 
 //Ruta para obtener todos los usuarios
-router.get("/",  usersGet);
-router.get("/user",  userGet);
-router.post("/",  userPost);
-router.delete("/",  userDelete);
-router.put("/",  userPut);
+router.get("/", usersGet);
+router.get("/user", userGet);
+router.get("/login", login);
+
+router.post("/", userPost);
+router.delete("/", userDelete);
+router.put("/", userPut);
 
 export default router;
