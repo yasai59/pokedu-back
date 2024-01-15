6cb61a946a8ffd4e939953174fabce7ba643bcf0
Yasai <alexvidalcasado@gmail.com>
2024-01-15 09:16:47 +0100

    [ADD] get activities from project

--- server/routes/activityRoute.js
+++ server/routes/activityRoute.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import { activityDelete, activityGet, activityPost, activityPut, activitiesGet, currentActivityUserGet, finishedActivityUserGet, activityPostMassive } from "../controllers/activityController.js";
+import {
+  activityDelete,
+  activityGet,
+  activityPost,
+  activityPut,
+  activitiesGet,
+  currentActivityUserGet,
+  finishedActivityUserGet,
+  activitiesGetByProject,
+  activityPostMassive,
+} from "../controllers/activityController.js";
 import verifyJWT from "../middlewares/verifyJWT.js";
 
 //Creamos la ruta
@@ -11,8 +21,9 @@ const router = Router();
 router.get("/", activityGet);
 router.get("/activity", activityGet);
 router.post("/", activityPost);
-router.put("/",activityPut);
-router.delete("/",activityDelete);
+router.put("/", activityPut);
+router.delete("/", activityDelete);
+router.get("/project", activitiesGetByProject);
 router.get("/currentActivitysUser", currentActivityUserGet);
 router.get("/finishedActivitysUser", finishedActivityUserGet);
 router.post("/activityPostMassive", activityPostMassive);
