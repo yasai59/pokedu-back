6228962f76bb6eb564a13545e550cf64ef5240b2
Yasai <alexvidalcasado@gmail.com>
2024-01-15 15:55:40 +0100

    [XD]

--- server/routes/projectRoute.js
+++ server/routes/projectRoute.js
@@ -8,6 +8,7 @@ import {
   projectsGet,
 } from "../controllers/projectController.js";
 import verifyJWT from "../middlewares/verifyJWT.js";
+import verifyTeacher from "../middlewares/verifyTeacher.js";
 
 //Creamos la ruta
 const router = Router();
@@ -15,7 +16,7 @@ const router = Router();
 //router.get("/", [verifyJWT], usersGet); ejemplo verificar
 
 //Rutas projectos
-router.get("/", [verifyJWT], projectsGet);
+router.get("/", [verifyJWT, verifyTeacher], projectsGet);
 router.get("/project", projectGet);
 router.post("/", projectPost);
 router.put("/", projectPut);
