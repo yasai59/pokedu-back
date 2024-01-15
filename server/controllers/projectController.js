a2ce442d6fa815dd321f91bf7aca82a70167906a
Yasai <alexvidalcasado@gmail.com>
2024-01-15 16:17:47 +0100

    quito un join

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -4,127 +4,119 @@ import dbQuery from "../db/dbConnection.js";
 
 //Obtener todos los projectos
 export const projectsGet = async (req, res) => {
-    let result;
-    try {
-      result = await dbQuery("SELECT * FROM PROJECTES;");
-    } catch (e) {
-      return res.status(400).json({
-        error: "Invalid query",
-      });
-    }
-  
-    res.json({
-      msg: result,
+  let result;
+  try {
+    result = await dbQuery("SELECT * FROM PROJECTES;");
+  } catch (e) {
+    return res.status(400).json({
+      error: "Invalid query",
     });
+  }
+
+  res.json({
+    msg: result,
+  });
 };
 
 //Borrar un projecto
 export const projectDelete = async (req, res) => {
-    const { projectId } = req.body;
-    let result;
-    try {
-        result = await dbQuery(`DELETE FROM PROJECTES WHERE id = '${projectId}';`);
-    } catch (e) {
-        console.log(e);
-        return res.status(400).json({
-        error: "Invalid query",
-        });
-    }
+  const { projectId } = req.body;
+  let result;
+  try {
+    result = await dbQuery(`DELETE FROM PROJECTES WHERE id = '${projectId}';`);
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json({
+      error: "Invalid query",
+    });
+  }
 
-    res.json({
-        msg: "ok",
-      });
+  res.json({
+    msg: "ok",
+  });
 };
 
 //Obtener un projecto en concreto
 export const projectGet = async (req, res) => {
-    const { projectId } = req.query;
-  
-    let result;
-    try {
-      result = await dbQuery(`SELECT * FROM PROJECTES WHERE id = ${projectId};`);
-    } catch (e) {
-      return res.status(400).json({
-        error: "Invalid query",
-      });
-    }
-  
-    res.json({
-      msg: result[0],
+  const { projectId } = req.query;
+
+  let result;
+  try {
+    result = await dbQuery(`SELECT * FROM PROJECTES WHERE id = ${projectId};`);
+  } catch (e) {
+    return res.status(400).json({
+      error: "Invalid query",
     });
-  };
-  
+  }
+
+  res.json({
+    msg: result[0],
+  });
+};
+
 //Crear un projecto
-  export const projectPost = async (req, res) => {
-    const { projectName} = req.body;
+export const projectPost = async (req, res) => {
+  const { projectName } = req.body;
 
-    let result;
-    try {
-      result = await dbQuery(
-        `INSERT INTO PROJECTES (nom) VALUES ('${projectName}');`
-      );
-    } catch (e) {
-      console.log(e);
-      return res.status(400).json({
-        error: "Invalid query",
-      });
-    }
-  
-    res.json({
-      msg: "ok",
+  let result;
+  try {
+    result = await dbQuery(
+      `INSERT INTO PROJECTES (nom) VALUES ('${projectName}');`
+    );
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json({
+      error: "Invalid query",
     });
-  };
+  }
 
-  //Modificar un projecto
-  export const projectPut = async (req, res) => {
-    const { projectId, projectName } = req.body;
-  
-    let result;
-    try {
-      result = await dbQuery(
-        `UPDATE PROJECTES SET nom = '${projectName}' WHERE id = '${projectId}'`
-      );
-    } catch (e) {
-      console.log(e);
-      return res.status(400).json({
-        error: "Invalid query",
-      });
-    }
-  
-    res.json({
-      msg: "ok",
+  res.json({
+    msg: "ok",
+  });
+};
+
+//Modificar un projecto
+export const projectPut = async (req, res) => {
+  const { projectId, projectName } = req.body;
+
+  let result;
+  try {
+    result = await dbQuery(
+      `UPDATE PROJECTES SET nom = '${projectName}' WHERE id = '${projectId}'`
+    );
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json({
+      error: "Invalid query",
     });
-  };
+  }
+
+  res.json({
+    msg: "ok",
+  });
+};
 
 //Conseguir todos los projectos de un usuario
-  export const projectUserGet = async (req, res) => {
+export const projectUserGet = async (req, res) => {
+  const { userId } = req.query;
 
-    const { userId} = req.query;
-  
-    let result;
-    try {
-      result = await dbQuery(
-        ` SELECT p.*
+  let result;
+  try {
+    result = await dbQuery(
+      ` SELECT p.*
         FROM PROJECTES p
         JOIN USUARIOS_PROJECTES up ON p.id = up.projecte
-        JOIN USUARIOS u ON up.alumne = u.id
-        WHERE u.id = '${userId}';
+        WHERE up.alumne = '${userId}' GROUP BY p.id;
         `
-      );
-    } catch (e) {
-      console.log(e);
-      return res.status(400).json({
-        error: "Invalid query",
-      });
-    }
-  
-    res.json({
-      msg: result[0],
+    );
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json({
+      error: "Invalid query",
     });
+  }
 
-
-  };
-
- 
-
-
+  res.json({
+    msg: result,
+  });
+};
